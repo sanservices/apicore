3ab6c7519725eeb3cd085b61018d23617945312e
disturb01 <80925537+disturb01@users.noreply.github.com>
2021-04-13 08:29:17 -0600

    Feature/break backwards compatibility (#2) (#3)
    * added v2
    
    * break backwards compatibility, because old apicore is under san-services
    
    * change v2 to v1, because it is v1 under sanservices
    
    * added some description
    
    Co-authored-by: Friedrich42 <murad@buglance.com>
    
    Co-authored-by: Murr42 <80788052+Murr42@users.noreply.github.com>
    Co-authored-by: Friedrich42 <murad@buglance.com>

---
+++ helper/helper.go
@@ -0,0 +1,41 @@
+package helper
+
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/sanservices/apicore/errs"
+	"net/http"
+)
+
+// httpResponse standard json response
+type successResponse struct {
+	Data interface{} `json:"data"`
+}
+
+// JSON response with error
+type errorResponse struct {
+	Errors []errs.ServiceError `json:"errors"`
+}
+
+// RespondError sends a json error response
+func RespondError(c echo.Context, httpStatusCode int, respErr error) error {
+	var response errorResponse
+	if sErrs, ok := respErr.(errs.ServiceError); ok {
+		response.Errors = append(response.Errors, sErrs)
+	} else {
+		switch httpStatusCode {
+		case http.StatusBadRequest:
+			response.Errors = append(response.Errors, errs.NewInputMalformedErr(respErr))
+		default:
+			response.Errors = append(response.Errors, errs.NewInternalErr(respErr))
+		}
+	}
+
+	return c.JSON(httpStatusCode, response)
+}
+
+// RespondOk sends a json success response
+func RespondOk(c echo.Context, data interface{}) error {
+	response := &successResponse{Data: data}
+
+	return c.JSON(http.StatusOK, response)
+}
---
+++ middleware/middleware.go
@@ -0,0 +1,78 @@
+package middleware
+
+import (
+	"context"
+	"github.com/dchest/uniuri"
+	"github.com/labstack/echo/v4"
+	logger "github.com/sanservices/apilogger/v2"
+	"net/http"
+	"time"
+)
+
+// SetCustomHeaders sets some custom headers
+func SetCustomHeaders(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		res := next(c)
+
+		id, ok := c.Request().Context().Value("x-request-id").(string)
+		if !ok {
+			id = ""
+		}
+
+		c.Response().Header().Set("X-Request-Id", id)
+
+		return res
+	}
+}
+
+// EnrichContext enriches the context and sets context to request
+func EnrichContext(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		r := c.Request()
+
+		id := r.Header.Get(logger.RequestIDKey)
+		key := r.Header.Get(logger.APIKEY)
+		session := r.Header.Get(logger.SessionIDKey)
+
+		if id == "" {
+			id = uniuri.New()
+		}
+
+		addr := r.Header.Get("x-real-ip")
+		if addr == "" {
+			if r.RemoteAddr != "" {
+				addr = r.RemoteAddr
+			}
+		}
+
+		ctx := r.Context()
+
+		ctx = context.WithValue(ctx, logger.APIKEY, key)
+		ctx = context.WithValue(ctx, logger.RequestIDKey, id)
+		ctx = context.WithValue(ctx, logger.RemoteAddrKey, addr)
+		ctx = context.WithValue(ctx, logger.SessionIDKey, session)
+		ctx = context.WithValue(ctx, logger.StartTime, time.Now())
+
+		c.SetRequest(r.WithContext(ctx)) // set context to request and request to echo context
+
+		return next(c)
+	}
+}
+
+// RequestLogger logs requests
+func RequestLogger(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		r := c.Request()
+
+		// Call next handler
+		err := next(c)
+
+		logger.InfoWF(r.Context(), logger.LogCatReqPath, &logger.Fields{
+			"status": http.StatusOK,
+			"url":    r.URL,
+			"method": r.Method,
+		})
+
+		return err
+	}
+}
